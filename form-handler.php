fec3a2c79e0033cce8f572f9cb1910878d02fd40
Aspirantsway <102422381+Aspirantsway@users.noreply.github.com>
2022-03-30 22:37:46 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST{'email'};
 $subject = $_POST{'subject'};
 $message = $_POST{'message'};
 
-$email_from= 'aspirantsway1@gmail.com';
+$email_from= 'aspirantsway.com';
 
 $email_subject= 'New Form Submission';
 
